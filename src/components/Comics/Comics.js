5567ffc0fb4c39c05994ca253f906f9a16d95235
Sergey Igoshin <VipReaLWeb@gmail.com>
2024-08-13 23:22:52 +0500

    add Error

--- src/components/Comics/Comics.js
+++ src/components/Comics/Comics.js
@@ -4,6 +4,8 @@ import { ROOT_INDEX } from '../../constants/root';
 import { getDataApi } from "../../utils/getDataApi";
 import { BASE_URL, URL_COMICS, URL_CHARACTERS, IMG_STANDARD_XLARGE, IMG_NOT_AVAILABLE } from '../../constants/api';
 
+import Error from '../Error/Error';
+
 class Comics {
     eventListener() {
         document.querySelectorAll('.comics__item').forEach(element => {
@@ -14,14 +16,12 @@ class Comics {
         })
     }
 
-    async render() {
-        const data = await getDataApi.getData(BASE_URL + URL_COMICS);
-        
+    renderComics(data) {
         let htmlContent = '';
 
         data.forEach(({ id, title, thumbnail: { path, extension } }) => {
-            
-            if ( path.lastIndexOf(IMG_NOT_AVAILABLE) === -1 ) {
+
+            if (path.lastIndexOf(IMG_NOT_AVAILABLE) === -1) {
                 const uri = BASE_URL + URL_COMICS + '/' + id + '/' + URL_CHARACTERS
                 const imgSrc = path + '/' + IMG_STANDARD_XLARGE + '.' + extension;
 
@@ -41,6 +41,12 @@ class Comics {
         `
 
         ROOT_INDEX.innerHTML = htmlWrapper;
+    }
+
+    async render() {
+        const data = await getDataApi.getData(BASE_URL + URL_COMICS);
+
+        data ? this.renderComics(data) : Error.render();
     }
 }
 
